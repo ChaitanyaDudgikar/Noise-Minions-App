c227a78447f670a8f92094f087db579e0729bdff
ChaitanyaDudgikar <55018992+ChaitanyaDudgikar@users.noreply.github.com>
2020-04-18 15:31:29 +0530

    Commit

---
+++ app/src/main/java/com/example/beproject/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.beproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler h=new Handler();
+        h.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Intent splash=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(splash);
+                finish();
+            }
+        },3000);
+    }
+}
